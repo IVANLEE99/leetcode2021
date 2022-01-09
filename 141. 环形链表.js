b1d817307ce007d6e3e2a054a4eace5de229c9ae
youngs <lixiang@popicorns.com>
2022-01-09 16:25:10 +0800

    feat:141. 环形链表

---
+++ 141. 环形链表.js
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  //     环形的链表遍历一定会在环的地方重复
+  // p1=p1.next
+  // p2 = p2.next.next
+  // 一快一慢遍历，
+  // 有环一定会重逢
+  let p1 = head;
+  let p2 = head;
+  while (p1 && p2 && p2.next) {
+    p1 = p1.next;
+    p2 = p2.next.next;
+    if (p1 === p2) {
+      return true;
+    }
+  }
+  return false;
+};
--- 2. 两数相加.js
+++ 2. 两数相加.js
@@ -25,13 +25,15 @@ var addTwoNumbers = function (l1, l2) {
     let v = ((p1 && p1.val) || 0) + ((p2 && p2.val) || 0) + carry;
     carry = Math.floor(v / 10);
     p3.val = v % 10;
-    p3.next = new ListNode(0);
-    p1 = p1.next;
-    p2 = p2.next;
-    p3 = p3.next;
+    p1 = p1 && p1.next;
+    p2 = p2 && p2.next;
+    if (p1 || p2) {
+      p3.next = new ListNode(0);
+      p3 = p3.next;
+    }
   }
   if (carry) {
-    p3.val = carry;
+    p3.next = new ListNode(carry);
   }
   return l3;
 };
