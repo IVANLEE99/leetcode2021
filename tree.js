3d746e31906e4957bb676562fa222b68921adfed
youngs <lixiang@popicorns.com>
2022-04-06 21:26:36 +0800

    feat:104. 二叉树的最大深度

---
+++ tree.js
@@ -0,0 +1,29 @@
+let treeData = {
+  val: "1",
+  left: {
+    val: "2",
+    left: {
+      val: "3",
+      left: null,
+      right: null,
+    },
+    right: {
+      val: "4",
+      left: {
+        val: "5",
+        left: null,
+        right: null,
+      },
+      right: null,
+    },
+  },
+  right: {
+    val: "6",
+    left: null,
+    right: {
+      val: "7",
+      left: null,
+      right: null,
+    },
+  },
+};
